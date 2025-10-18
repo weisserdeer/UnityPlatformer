279b8b804888405453566c5f9de1f46614d264f9
Alessia Russo <weisserdeer@gmail.com>
2025-10-18 17:55:26 +0300

    Base commit

---
+++ Assets/PixelCrew/Hero.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField] private float _speed; //unity will save the value of this field into the scene
+
+    private float _xdirection;
+    private float _ydirection;
+
+    public void SetXDirection(float xdirection)
+    {
+        _xdirection = xdirection;
+    }
+    public void SetYDirection(float ydirection)
+    {
+        _ydirection = ydirection;
+    }
+
+    private void Update()
+    {
+        
+        if (_xdirection != 0)
+        {
+            var delta = _xdirection * _speed * Time.deltaTime; //deltaTime - the time since the last frame
+            var newXPosition = transform.position.x + delta;
+            transform.position = new Vector2(newXPosition, transform.position.y);
+        }
+
+        if (_ydirection != 0)
+        {
+            var delta = _ydirection * _speed * Time.deltaTime; //deltaTime - the time since the last frame
+            var newYPosition = transform.position.y + delta;
+            transform.position = new Vector2(transform.position.x, newYPosition);
+        }
+
+    }
+
+    public void SaySomething()
+    {
+        Debug.Log("Something");
+    }
+}
---
+++ Assets/PixelCrew/HeroInputReader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HeroInputReader : MonoBehaviour
+{
+    [SerializeField] private Hero _hero;
+
+    public void OnHorizontalMovement(InputAction.CallbackContext context)
+    {
+        var xdirection = context.ReadValue<float>();
+        _hero.SetXDirection(xdirection);
+    }
+    public void OnVerticalMovement(InputAction.CallbackContext context)
+    {        
+        var ydirection = context.ReadValue<float>();
+        _hero.SetYDirection(ydirection);
+    }
+
+    public void OnSaySomething(InputAction.CallbackContext context)
+    {
+        if (context.canceled)
+        {
+            _hero.SaySomething();
+        }
+    }
+}
